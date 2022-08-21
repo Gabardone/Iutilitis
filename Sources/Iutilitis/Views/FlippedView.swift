bd7e2d15b374a3204d8d44114ab3a063f7f42714
Óscar Morales Vivó <oscarmv@mac.com>
2022-08-21 00:18:48 -0700

    Added FlippedView, the first of many utility NSView subclasses.

---
+++ Sources/Iutilitis/Views/FlippedView.swift
@@ -0,0 +1,14 @@
+//
+//  FlippedView.swift
+//
+//
+//  Created by Óscar Morales Vivó on 8/21/22.
+//
+
+import Cocoa
+
+public class FlippedView: NSView {
+    override public var isFlipped: Bool {
+        true
+    }
+}
