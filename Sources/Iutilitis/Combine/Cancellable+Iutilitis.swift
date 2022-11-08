8ef5a243be5f50b0a1c752180a2ce658126aee3e
Óscar Morales Vivó <oscarmv@mac.com>
2022-11-07 22:45:54 -0800

    Added Cancellable utilities.

---
+++ Sources/Iutilitis/Combine/Cancellable+Iutilitis.swift
@@ -0,0 +1,28 @@
+//
+//  Cancellable.swift
+//
+//
+//  Created by Óscar Morales Vivó on 11/7/22.
+//
+
+import Combine
+import Foundation
+
+public extension Cancellable {
+    /**
+     Avoids needing to use `AnyCancellable` type to store subscriptions in arrays and other simple collections.
+     */
+    func store<C>(in collection: inout C) where C: RangeReplaceableCollection, C.Element == any Cancellable {
+        collection.append(self)
+    }
+}
+
+public extension Cancellable where Self: Hashable {
+    /**
+     May be useful to avoid using `AnyCancellable` to store subscriptions but only works if they are all of the same
+     kind.
+     */
+    func store(in set: inout Set<Self>) {
+        set.insert(self)
+    }
+}
